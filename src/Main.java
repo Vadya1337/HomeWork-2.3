9019686aed9c7fb452c2810626561f0cfcbde3e2
Вадя <vadim.filatov.02@gmail.com>
2024-11-04 00:29:49 +0700

    Сделал дз

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,29 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Car car = new Car(" Car 1", 4);
+        Car car2 = new Car(" Car 2", 4);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Truck truck = new Truck(" truck 1", 6);
+        Truck truck2 = new Truck(" truck 2", 8);
+
+
+        Bicycle bicycle = new Bicycle(" bicycle1", 2);
+        Bicycle bicycle2 = new Bicycle(" bicycle2", 2);
+Transport[] transports = {
+        car,
+        car2,
+        truck,
+        truck2,
+        bicycle,
+        bicycle2
+};
+
+        ServiceStation station = new ServiceStation();
+        for (Transport transport: transports){
+            station.check(transport);
+            System.out.println();
         }
+
     }
 }
